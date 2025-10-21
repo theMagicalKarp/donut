8c80d2085d4fff55ea60cb68471f6ca48fa9617a
Robert Sheehy <gameboy1092@gmail.com>
2025-10-21 09:45:26 -0500

    Add camera indicators/zoom control

--- src/main.zig
+++ src/main.zig
@@ -19,6 +19,12 @@ pub fn main() !void {
     defer std.debug.assert(gpa.deinit() == .ok);
     const allocator = gpa.allocator();
 
+    var scroll_buffer_x = std.io.Writer.Allocating.init(allocator);
+    defer scroll_buffer_x.deinit();
+
+    var scroll_buffer_y = std.io.Writer.Allocating.init(allocator);
+    defer scroll_buffer_y.deinit();
+
     var buffer: [1024]u8 = undefined;
     var tty = try vaxis.Tty.init(&buffer);
     defer tty.deinit();
@@ -82,13 +88,14 @@ pub fn main() !void {
 
     // const g = repeat;
     // const g = bouncing_sphere;
+    // const g = g1;
     const g = g1;
 
     const GeometryScene = Scene(Geometry);
     const shading = Shading.new(light_position, gamma);
     const scene = GeometryScene.new(shading);
 
-    const camera_distance: f64 = 2.0;
+    var camera_distance: f64 = 2.0;
     var camera_theta: f64 = 0.0;
     var camera_phi: f64 = 1.57;
 
@@ -122,6 +129,7 @@ pub fn main() !void {
                         );
                     } else if (key.matches('d', .{})) {
                         camera_theta = camera_theta - 0.05;
+
                         camera.position = Camera.orbit(
                             camera.look_at,
                             camera_distance,
@@ -129,7 +137,7 @@ pub fn main() !void {
                             camera_phi,
                         );
                     } else if (key.matches('w', .{})) {
-                        camera_phi = std.math.clamp(camera_phi + 0.05, 0.1, 3.04);
+                        camera_phi = std.math.clamp(camera_phi - 0.05, 0.1, 3.04);
                         camera.position = Camera.orbit(
                             camera.look_at,
                             camera_distance,
@@ -137,7 +145,32 @@ pub fn main() !void {
                             camera_phi,
                         );
                     } else if (key.matches('s', .{})) {
-                        camera_phi = std.math.clamp(camera_phi - 0.05, 0.1, 3.04);
+                        camera_phi = std.math.clamp(camera_phi + 0.05, 0.1, 3.04);
+                        camera.position = Camera.orbit(
+                            camera.look_at,
+                            camera_distance,
+                            camera_theta,
+                            camera_phi,
+                        );
+                    } else if (key.matches('r', .{})) {
+                        camera_theta = 0.0;
+                        camera_phi = 1.57;
+                        camera.position = Camera.orbit(
+                            camera.look_at,
+                            camera_distance,
+                            camera_theta,
+                            camera_phi,
+                        );
+                    } else if (key.matches('z', .{ .shift = false })) {
+                        camera_distance = camera_distance - 0.05;
+                        camera.position = Camera.orbit(
+                            camera.look_at,
+                            camera_distance,
+                            camera_theta,
+                            camera_phi,
+                        );
+                    } else if (key.matches('z', .{ .shift = true })) {
+                        camera_distance = camera_distance + 0.05;
                         camera.position = Camera.orbit(
                             camera.look_at,
                             camera_distance,
@@ -185,15 +218,65 @@ pub fn main() !void {
             .{ .wrap = .grapheme },
         );
 
-        const child2 = win.child(.{
+        _ = win.child(.{
             .x_off = win.width / 2 - 2,
             .y_off = 0,
             .width = 25,
             .height = 1,
-        });
-
-        _ = child2.printSegment(
+        }).printSegment(
             .{ .text = " demo ", .style = .{
+                .bold = true,
+                .fg = .{ .index = 4 },
+            } },
+            .{ .wrap = .grapheme },
+        );
+
+        scroll_buffer_x.clearRetainingCapacity();
+        const x_progress: i32 = @intFromFloat(
+            @mod(std.math.pi - camera_theta, std.math.tau) / std.math.tau * @as(f64, @floatFromInt(win.width - 2)),
+        );
+        for (0..win.width - 2) |i| {
+            if (i == x_progress) {
+                _ = try scroll_buffer_x.writer.write("▲");
+            } else {
+                _ = try scroll_buffer_x.writer.write(" ");
+            }
+        }
+
+        _ = win.child(.{
+            .x_off = 1,
+            .y_off = 1,
+            .width = win.width - 2,
+            .height = 1,
+        }).printSegment(
+            .{ .text = scroll_buffer_x.written(), .style = .{
+                .bold = true,
+                .fg = .{ .index = 4 },
+            } },
+            .{ .wrap = .grapheme },
+        );
+
+        scroll_buffer_y.clearRetainingCapacity();
+
+        const y_progress: i32 = @intFromFloat(
+            @mod(camera_phi - 0.1, 3.04) / 3.04 * @as(f64, @floatFromInt(win.height - 2)),
+        );
+
+        for (0..win.height - 2) |i| {
+            if (i == y_progress) {
+                _ = try scroll_buffer_y.writer.write("◀");
+            } else {
+                _ = try scroll_buffer_y.writer.write(" ");
+            }
+        }
+
+        _ = win.child(.{
+            .x_off = 1,
+            .y_off = 1,
+            .width = 1,
+            .height = win.height - 2,
+        }).printSegment(
+            .{ .text = scroll_buffer_y.written(), .style = .{
                 .bold = true,
                 .fg = .{ .index = 4 },
             } },
