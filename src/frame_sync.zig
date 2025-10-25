7319cf1abe6679d12e4a3295e996e85ef775984b
Robert Sheehy <gameboy1092@gmail.com>
2025-10-25 16:52:51 -0500

    Improve how FPS syncing works

--- src/frame_sync.zig
+++ src/frame_sync.zig
@@ -3,12 +3,10 @@ const std = @import("std");
 pub const FrameSync = struct {
     target: u64,
     frame_count: usize,
-    sample_interval: u64,
-    last_sample: u64,
-    measured_fps: f64,
+    fps: f64,
+    fps_sample_interval: u64,
+    fps_sample_timer: std.time.Timer,
     timer: std.time.Timer,
-    frame_start: u64,
-    frame_end: u64,
 
     const Self = @This();
 
@@ -17,35 +15,34 @@ pub const FrameSync = struct {
         return Self{
             .target = target,
             .frame_count = 0,
-            .sample_interval = std.time.ns_per_s,
-            .last_sample = 0.0,
-            .measured_fps = 0.0,
+            .fps_sample_interval = std.time.ns_per_s,
+            .fps_sample_timer = try std.time.Timer.start(),
+            .fps = 0.0,
             .timer = try std.time.Timer.start(),
-            .frame_start = 0,
-            .frame_end = 0,
         };
     }
 
     pub fn start(self: *Self) void {
-        self.frame_start = self.timer.read();
+        self.timer.reset();
     }
 
     pub fn end(self: *Self) void {
-        self.frame_end = self.timer.read();
         self.frame_count = self.frame_count + 1;
 
-        const sample_delta = self.frame_end - self.last_sample;
-        if (sample_delta >= self.sample_interval) {
-            self.measured_fps = @as(f64, @floatFromInt(self.frame_count)) * @as(f64, @floatFromInt(self.sample_interval)) / @as(f64, @floatFromInt(sample_delta));
+        const sample_read = self.fps_sample_timer.read();
+        if (sample_read >= self.fps_sample_interval) {
+            self.fps = @as(f64, @floatFromInt(self.frame_count)) * @as(f64, @floatFromInt(self.fps_sample_interval)) / @as(f64, @floatFromInt(sample_read));
             self.frame_count = 0;
-            self.last_sample = self.frame_end;
+            self.fps_sample_timer.reset();
         }
     }
 
     pub fn wait(self: *Self) void {
-        const dt_ns: u64 = self.frame_end - self.frame_start;
-        if (dt_ns < self.target) {
-            std.Thread.sleep(self.target - dt_ns);
+        while (self.timer.read() <= self.target) {
+            std.Thread.sleep(@min(
+                std.time.ns_per_ms,
+                self.target - self.timer.read(),
+            ));
         }
     }
 };
